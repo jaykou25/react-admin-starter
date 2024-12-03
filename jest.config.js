56c22686eba6460827ba70ef8c7c1a34b8419182
jay.kou <jay.kou@qq.com>
2024-12-03 09:11:52 +0800

    fix: 完善测试框架

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-const config = {
-  // 注意这条规则仅针对 pnpm. 因为 pnpm 的 node_moduels 里的包都是一个软链指向 node_moduels 下的 .pnpm
-  // 如果是非 pnpm 的包管理器, 规则是 'node_modules\/(?!(lodash-es|bar))'
-  transformIgnorePatterns: ['node_modules\/\.pnpm\/(?!(lodash-es|bar))'],
-  testEnvironment: 'jsdom',
-}
-
-module.exports = config
